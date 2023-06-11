9e15f2b41fe1b7a460576f2282fab1a8d5dbab99
Eastpond <ldyallo@naver.com>
2023-06-11 22:55:29 +0900

    player
    import

--- Dungeon/Assets/02.Scripts/WallCollision.cs
+++ Dungeon/Assets/02.Scripts/WallCollision.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallCollision : MonoBehaviour
+public class WallCollision : RoomBehaviour
 {
     // Start is called before the first frame update
     void Start()
